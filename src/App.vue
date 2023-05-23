6b1ad99f685a7cd413f54bdd958e6d0510056aa9
unpieceof <unpieceof@gmail.com>
2023-05-23 10:50:14 +0900

    [FE] Sidebar 레이아웃 구성

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <template>
   <div class="flex h-screen">
-    <div class="w-1/4 bg-gray-200">
-      <i class="fa-solid fa-water-ladder text-2xl text-primary mt-5 ml-10"></i>
-      <i class="fa-solid fa-house-chimney"></i>
+    <div class="w-1/5 mt-3 ml-20">
+      <!-- twitter logo -->
+      <i class="fa-solid fa-water-ladder text-3xl text-primary ml-3"></i>
+      <!-- sidemenu icons -->
+      <div class="flex flex-col items-start">
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-solid fa-house-chimney fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Home</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-solid fa-hashtag fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Explore</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-regular fa-bell fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Notifications</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-regular fa-envelope fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Messages</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-solid fa-list fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Lists</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-regular fa-bookmark fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Bookmarks</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-regular fa-user fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">Profile</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+          <i class="fa-solid fa-ellipsis-h fa-fw text-2xl"></i>
+          <span class="ml-4 text-xl">More</span>
+        </div>
+      </div>
+      <button class="mt-5 bg-primary text-white w-60 h-14 rounded-full font-bold hover:bg-dark">Tweet</button>
     </div>
     <div class="flex-1 bg-red-200"></div>
 </div>
