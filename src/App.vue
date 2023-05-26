ffbbc66f232cc0d0e41db503e86b61db6dc68570
unpieceof <unpieceof@gmail.com>
2023-05-26 15:54:08 +0900

    사이드메뉴에 프로필 버튼 추가

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,64 @@
 <template>
   <div class="flex h-screen">
-    <div class="w-1/5 mt-4 ml-20">
-      <!-- twitter logo -->
-      <i class="fa-solid fa-water-ladder text-4xl text-primary ml-3"></i>
-      <!-- sidemenu icons -->
-      <div class="flex flex-col items-start">
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-solid fa-house-chimney fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Home</span>
+    <!-- side section -->
+    <div class="w-1/5 mt-4 ml-10 flex flex-col justify-between">
+      <div>
+        <!-- twitter logo -->
+        <i class="fa-solid fa-water-ladder text-4xl text-primary ml-3"></i>
+        <!-- sidemenu icons -->
+        <div class="flex flex-col items-start space-y-3">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-solid fa-house-chimney fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Home</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-solid fa-hashtag fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Explore</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-regular fa-bell fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Notifications</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-regular fa-envelope fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Messages</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-solid fa-list fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Lists</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-regular fa-bookmark fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Bookmarks</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-regular fa-user fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">Profile</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
+            <i class="fa-solid fa-ellipsis-h fa-fw text-2xl"></i>
+            <span class="ml-4 text-xl">More</span>
+          </div>
         </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-solid fa-hashtag fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Explore</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-regular fa-bell fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Notifications</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-regular fa-envelope fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Messages</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-solid fa-list fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Lists</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-regular fa-bookmark fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Bookmarks</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-regular fa-user fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">Profile</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-1 rounded-full mt-5">
-          <i class="fa-solid fa-ellipsis-h fa-fw text-2xl"></i>
-          <span class="ml-4 text-xl">More</span>
+        <!-- tweet button -->
+        <div class="mr-10">
+          <button class="mt-5 bg-primary text-white w-full h-14 rounded-full font-bold hover:bg-dark">Tweet</button>
         </div>
       </div>
-      <button class="mt-5 bg-primary text-white w-60 h-14 rounded-full font-bold hover:bg-dark">Tweet</button>
+        <!-- profile button -->
+        <div class="mr-10 mb-5">
+          <button class="mt-5 px-2 py-2 w-full h-14 rounded-full font-bold hover:bg-gray-100 flex items-center">
+            <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full">
+            <div class="ml-4">
+              <div class="text-sm font-bold text-left">A</div>
+              <div class="text-xs text-gray-500 font-medium">@anoffif</div>
+            </div>
+            <i class="fa-solid fa-ellipsis-h fa-fw text-lg ml-auto "></i>
+          </button>
+        </div>
     </div>
-    <div class="flex-1">home</div>
-</div>
+    <div class="flex-1 bg-gray-50">home</div>
+  </div>
 </template>
 
 <script>
