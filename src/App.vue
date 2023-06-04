0467ada2e0ca895571d3e397d49043a100de625d
unpieceof <unpieceof@gmail.com>
2023-06-04 17:03:45 +0900

    [FE] 트윗 입력창 및 트윗 부분 추가

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@
           <button class="hidden lg:flex mt-5 px-2 py-2 w-full h-14 rounded-full font-bold hover:bg-gray-100 flex items-center">
             <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full">
             <div class="lg:ml-4 hidden lg:block">
-              <div class="text-sm font-bold text-left">A</div>
+              <div class="text-sm font-bold text-left">𝐀</div>
               <div class="text-xs text-gray-500 font-medium">@anoffif</div>
             </div>
             <i class="fa-solid fa-ellipsis-h fa-fw text-lg ml-auto hover:text-primary hidden lg:block"></i>
@@ -55,7 +55,55 @@
           <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer hover:opacity-50">
         </div>
     </div>
-    <div class="flex-1">home</div>
+    <div class="flex-1">
+      <div class="flex h-screen">
+        <!-- main section -->
+        <div class="flex-1 border-r border-gray-100">
+          <div class="flex flex-col">
+            <!-- page title -->
+            <div class="border-b border-gray-100 px-4 py-3 text-xl font-bold">
+              Home
+            </div>
+            <!-- tweeting section -->
+            <div class="flex p-3 border-b border-gray-100">
+              <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer">
+              <div class="flex flex-1 flex-col ml-2">
+                <textarea class="w-full text-lg font-bold focus:outline-none resize-none" 
+                          placeholder="What is happening?!"></textarea>
+                <div class="text-right">
+                  <button class="hover:bg-dark bg-primary text-white text-sm font-bold rounded-full px-4 py-2">Tweet</button>
+                </div>
+              </div>
+            </div>
+            <!-- tweets -->
+            <div>
+              <div class="flex p-3 border-b border-gray-100">
+                <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer">
+                <div class="flex flex-1 flex-col ml-3">
+                  <div class="font-light space-x-1">
+                    <span class="font-bold">𝐀</span>
+                    <span class="text-gray-500">@anoffif</span>
+                    <span class="text-gray-500">·</span>
+                    <span class="text-gray-500">17m</span>
+                  </div>
+                  <!-- tweet body -->
+                  <div>벌써 월요일이야.. ..</div>
+                  <!-- tweet actions -->
+                  <div class="flex">
+                    <i class="far fa-comment"></i>
+                    <i class="far fa-comment"></i>
+                    <i class="far fa-comment"></i>
+                    <i class="far fa-comment"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- trend section -->
+        <div class="w-2/5">trend</div>
+      </div>
+    </div>
   </div>
 </template>
 
