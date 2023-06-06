5b2b46442e36293b92e2c3fc1bbb436561f00027
unpieceof <unpieceof@gmail.com>
2023-06-06 23:07:52 +0900

    component 분리

---
+++ src/components/Trends.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hidden lg:block lg:w-1/3 lg:mr-20 px-4 py-2">
+          <!-- twitter search input -->
+          <div class="relative mb-5">
+            <input type="text" 
+            placeholder="Search Twitter"
+            class="pl-10 rounded-full w-full p-2 bg-gray-100 text-sm focus:outline-none focus:ring-1 focus:ring-primary focus:bg-white" />
+            <i class="fas fa-search absolute left-0 mt-2 ml-3 text-sm text-primary"></i>
+          </div>
+          <!-- trend for me -->
+          <div class="w-full rounded-xl bg-gray-50 flex flex-col">
+            <div class="flex items-center justify-between p-3 border-b border-gray-100">
+              <span class="text-lg font-bold">Trends for you</span>
+              <i class="text-center fas fa-gear text-lg text-primary hover:bg-blue-50 px-2 py-1 rounded-full"></i>
+            </div>
+            <!-- dummy trends -->
+            <div class="border-b border-gray-100 hover:bg-gray-100 px-4 py-2 cursor-pointer">
+              <div class="flex">
+                <div class="flex-1 flex flex-col">
+                  <div class="text-gray-500 text-sm">Trending in South Korea</div>
+                  <div class="font-bold">개교기념일</div>
+                  <div class="text-gray-500 text-sm">2,016 Tweets</div>
+                </div>
+                <i class="fas fa-ellipsis-h text-gray-500 hover:text-primary"></i>
+              </div>
+            </div>
+            <!-- dummy end -->
+            <div class="p-4 hover:bg-gray-100 rounded-xl  cursor-pointer">
+              <span class="text-primary">Show more</span>
+            </div>
+          </div>
+          <!-- follow recommend -->
+          <div class="w-full rounded-xl bg-gray-50 flex flex-col mt-5">
+            <div class="p-3 border-b border-gray-100">
+              <span class="text-lg font-bold">Who to follow</span>
+            </div>
+            <!-- dummy user -->
+            <div class="border-b border-gray-100 hover:bg-gray-100 px-4 py-2">
+              <div class="flex items-center">
+                <div>
+                  <img src="https://picsum.photos/200" class="w-10 h-10 rounded-full mr-2">
+                </div>
+                <div class="flex-1 flex flex-col">
+                  <div class="font-bold cursor-pointer hover:underline">앙뇽</div>
+                  <div class="text-gray-500 text-sm">@hi_friend</div>
+                </div>
+                <div>
+                  <button class="bg-black text-white text-sm font-bold hover:bg-dark px-4 py-1.5 rounded-full">Follow</button>
+                </div>
+              </div>
+            </div>
+            <!-- dummy end -->
+            <div class="p-4 hover:bg-gray-100 rounded-xl cursor-pointer">
+              <span class="text-primary">Show more</span>
+            </div>
+          </div>
+        </div>
+</template>
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,66 @@
+<template>
+    <!-- main part -->
+    <div class="flex-1 border-r border-gray-100  overflow-y-auto">
+          <div class="flex flex-col">
+            <!-- page title -->
+            <div class="border-b border-gray-100 px-4 py-3 text-xl font-bold">
+              Home
+            </div>
+            <!-- tweeting part -->
+            <div class="flex p-3 border-b border-gray-100">
+              <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer">
+              <div class="flex flex-1 flex-col ml-2">
+                <textarea class="w-full text-lg font-bold focus:outline-none resize-none" 
+                          placeholder="What is happening?!"></textarea>
+                <div class="text-right">
+                  <button class="hover:bg-dark bg-primary text-white text-sm font-bold rounded-full px-4 py-2">Tweet</button>
+                </div>
+              </div>
+            </div>
+            <!-- tweets -->
+            <div>
+              <div class="flex p-3 border-b border-gray-100">
+                <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer">
+                <div class="flex flex-1 flex-col ml-3 space-y-1">
+                  <div class="font-light space-x-1">
+                    <span class="font-bold">𝐀</span>
+                    <span class="text-gray-500">@anoffif</span>
+                    <span class="text-gray-500">·</span>
+                    <span class="text-gray-500">17m</span>
+                  </div>
+                  <!-- tweet body -->
+                  <div>벌써 월요일이야.. ..</div>
+                  <!-- tweet actions -->
+                  <div class="flex justify-between">
+                    <div class="text-gray-500 hover:text-primary">
+                      <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
+                      <span class="ml-2 text-sm">2</span>
+                    </div>
+                    <div class="text-gray-500 hover:text-green-500">
+                      <i class="fas fa-retweet hover:bg-green-50 rounded-full p-2"></i>
+                      <span class="ml-2 text-sm">5</span>
+                    </div>
+                    <div class="text-gray-500 hover:text-pink-500">
+                      <i class="far fa-heart hover:bg-pink-50 rounded-full p-2"></i>
+                      <span class="ml-2 text-sm">32</span>
+                    </div>
+                    <div class="text-gray-500 hover:text-primary">
+                      <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+    <!-- trend part -->
+    <Trends />
+</template>
+
+<script>
+import Trends from '../components/Trends.vue'
+export default {
+  components: { Trends },
+  setup() {},
+}
+</script>
